f7d0941f52f279068a92e429d8dbe2eb1925808d
David Garcia <david_r_garcia@homedepot.com>
2018-12-27 10:26:23 -0500

    Initial Commit

---
+++ ApolloUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ApolloUI
+//
+//  Created by David Garcia on 12/27/18.
+//  Copyright © 2018 David Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ApolloUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ApolloUI
+//
+//  Created by David Garcia on 12/27/18.
+//  Copyright © 2018 David Garcia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
