96440f04046232d6064d27c1954d5695be98d032
David Garcia <david_r_garcia@homedepot.com>
2019-01-07 14:25:59 -0500

    Replaced DataSource binding with Cocoa bindings

--- ApolloUI/ViewController.swift
+++ ApolloUI/ViewController.swift
@@ -13,39 +13,20 @@ typealias Story = TestQueryQuery.Data.Me.Project.Story
 class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate{
     let client = (NSApplication.shared.delegate as? AppDelegate)?.apollo
     @IBOutlet var arrayController: NSArrayController!
-    @objc public var stories = NSArray()
+    private var stories = NSArray()
     @IBOutlet weak var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        tableView.dataSource = self
+        //Fetches query found in test.graphql
         let query = TestQueryQuery()
         client?.fetch(query: query){ (result, error) in
-            debugPrint(result?.data?.me?.project?.stories!.compactMap{$0})
-            self.stories = NSArray(array: result?.data?.me?.project?.stories!.compactMap{$0} ?? [Story(createdAt: "now", description: "test")])
-            self.tableView.reloadData()
-
-//            self.data?.me?.project?.stories?.compactMap{$0?.description as? String}.forEach{print($0)}
+//            debugPrint(result?.data?.me?.project?.stories!.compactMap{$0})
+            
+            //Right now the graphQL
+            self.stories = NSArray(array: result?.data?.me?.project?.stories!.compactMap{$0?.resultMap} ?? [Story(createdAt: "now", description: "test").resultMap])
+            self.arrayController.content = self.stories
         }
-    }
-    
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return stories.count
-    }
-    
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        var str: String?
-        switch tableColumn?.title {
-        case "Description":
-                        str = (stories[row] as! Story).description
-        case "Created At":
-                        str = (stories[row] as! Story).createdAt
-        default:
-            str = "oops"
-        }
-        print(str!)
-        return str
-//        let cell = tableView.
     }
 }
 
